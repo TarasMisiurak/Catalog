9b726499f3d113a8c1c50c52bad6cb7452b514b0
TarasMisyurak <tarasmisjyrac@gmail.com>
2017-02-12 19:07:49 +0200

    Added favorites assosiations to user and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
 	belongs_to :category
 	belongs_to :user
+	has_many :favorite_posts
+	has_many :favorited_by, through: :favorite_posts, source: :user
 
 	has_attached_file :image, styles: { thumb: "64x64#", small: "100x100#", medium: "350x350#", large: "700x700>" },
 					:url  => "/assets/products/:id/:style/:basename.:extension",

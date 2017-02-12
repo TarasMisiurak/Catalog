0a1fb1dd14910d3fd3203aff9c9beab8186515e3
TarasMisyurak <tarasmisjyrac@gmail.com>
2017-02-12 20:27:49 +0200

    Added comment functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	has_many :posts
 	has_many :favorite_posts
 	has_many :favorites, through: :favorite_posts, source: :post
+	has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
